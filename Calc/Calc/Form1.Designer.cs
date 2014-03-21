328fb61f01d208ceb121d47c5845d4d936c663f9
Ksenia0 <kseniya@mail.ru>
2014-03-21 11:07:46 +0700

    created calculator

---
+++ Calc/Calc/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addition = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.subtraction = new System.Windows.Forms.Button();
+            this.multiplication = new System.Windows.Forms.Button();
+            this.inputNomberTwo = new System.Windows.Forms.TextBox();
+            this.inputNomberOne = new System.Windows.Forms.TextBox();
+            this.res = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // addition
+            // 
+            this.addition.Location = new System.Drawing.Point(12, 126);
+            this.addition.Name = "addition";
+            this.addition.Size = new System.Drawing.Size(75, 23);
+            this.addition.TabIndex = 0;
+            this.addition.Text = "Сложение";
+            this.addition.UseVisualStyleBackColor = true;
+            this.addition.Click += new System.EventHandler(this.addition_Click);
+            // 
+            // divide
+            // 
+            this.divide.Location = new System.Drawing.Point(163, 155);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(75, 23);
+            this.divide.TabIndex = 1;
+            this.divide.Text = "Деление";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // subtraction
+            // 
+            this.subtraction.Location = new System.Drawing.Point(12, 155);
+            this.subtraction.Name = "subtraction";
+            this.subtraction.Size = new System.Drawing.Size(75, 23);
+            this.subtraction.TabIndex = 2;
+            this.subtraction.Text = "Вычитание";
+            this.subtraction.UseVisualStyleBackColor = true;
+            this.subtraction.Click += new System.EventHandler(this.subtraction_Click);
+            // 
+            // multiplication
+            // 
+            this.multiplication.Location = new System.Drawing.Point(163, 126);
+            this.multiplication.Name = "multiplication";
+            this.multiplication.Size = new System.Drawing.Size(75, 23);
+            this.multiplication.TabIndex = 3;
+            this.multiplication.Text = "Умножение";
+            this.multiplication.UseVisualStyleBackColor = true;
+            this.multiplication.Click += new System.EventHandler(this.multiplication_Click);
+            // 
+            // inputNomberTwo
+            // 
+            this.inputNomberTwo.Location = new System.Drawing.Point(12, 38);
+            this.inputNomberTwo.Name = "inputNomberTwo";
+            this.inputNomberTwo.Size = new System.Drawing.Size(260, 20);
+            this.inputNomberTwo.TabIndex = 4;
+            // 
+            // inputNomberOne
+            // 
+            this.inputNomberOne.Location = new System.Drawing.Point(12, 12);
+            this.inputNomberOne.Name = "inputNomberOne";
+            this.inputNomberOne.Size = new System.Drawing.Size(260, 20);
+            this.inputNomberOne.TabIndex = 5;
+            // 
+            // res
+            // 
+            this.res.Location = new System.Drawing.Point(12, 64);
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(260, 20);
+            this.res.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.inputNomberOne);
+            this.Controls.Add(this.inputNomberTwo);
+            this.Controls.Add(this.multiplication);
+            this.Controls.Add(this.subtraction);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.addition);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addition;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button subtraction;
+        private System.Windows.Forms.Button multiplication;
+        private System.Windows.Forms.TextBox inputNomberTwo;
+        private System.Windows.Forms.TextBox inputNomberOne;
+        private System.Windows.Forms.TextBox res;
+    }
+}
+
---
+++ Calc/Calc/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(inputNomberOne.Text);
+            double second = Convert.ToDouble(inputNomberTwo.Text);
+            double result = first/second;
+            res.Text = result.ToString();
+
+
+
+        }
+
+        private void multiplication_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(inputNomberOne.Text);
+            double second = Convert.ToDouble(inputNomberTwo.Text);
+            double result = first*second;
+            res.Text = result.ToString();
+
+        }
+
+        private void addition_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(inputNomberOne.Text);
+            double second = Convert.ToDouble(inputNomberTwo.Text);
+            double result = first + second;
+            res.Text = result.ToString();
+        }
+
+        private void subtraction_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(inputNomberOne.Text);
+            double second = Convert.ToDouble(inputNomberTwo.Text);
+            double result = first - second;
+            res.Text = result.ToString();
+        }
+    }
+}
